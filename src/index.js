786b6b1d95b5fb64df72a18135240f30914030bf
Clement <clementduchemin19@gmail.com>
2023-09-19 15:25:04 +0200

    npm build

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import '../src/scss/styles.scss';//import scss
 
 function component() {
     const element = document.createElement('div');
+    element.classList.add('container');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'onepoint'], ' ');
   
     return element;
   }
